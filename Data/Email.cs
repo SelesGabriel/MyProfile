5ce7d1f26e4d1c32636159f29ec3516d8cd250c8
selesgabriel <selesgabriel@gmail.com>
2020-05-28 11:43:31 -0300

    Versão faltando a correção do envio de email

---
+++ Data/Email.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorServerProfile.Data
+{
+    public class Email
+    {
+        [Required(ErrorMessage = "Campo obrigatório.")]
+        [MaxLength(30, ErrorMessage = "Tamanho máximo é de 30 caracteres.")]
+        public string Nome { get; set; }
+        [Required(ErrorMessage = "Campo obrigatório.")]
+        [EmailAddress(ErrorMessage = "Informe um email válido")]
+        public string EndEmail { get; set; }
+        [Required(ErrorMessage = "Campo obrigatório.")]
+        [MaxLength(300, ErrorMessage = "O limite de caracteres é de 300")]
+        public string Mensagem { get; set; } = "";
+        
+    }
+}
